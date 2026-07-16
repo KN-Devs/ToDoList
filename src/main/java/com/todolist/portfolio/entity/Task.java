365a594c4a09ed1e0d1d1c7208acc8a62cad0949
KN-Dev <vincent.devault1@gmail.com>
2026-07-16 13:59:14 +0200

    feat: entités JPA User, Task et enums Role, TaskStatus

---
+++ src/main/java/com/todolist/portfolio/entity/Task.java
@@ -0,0 +1,106 @@
+package com.todolist.portfolio.entity;
+
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+
+@Entity
+@Table(name = "task")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "task_id")
+    private Integer id;
+
+    @Column(nullable = false)
+    private String nom;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private TaskStatus status;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public Task() {
+    }
+
+    public Task(Integer id, String nom, String description, TaskStatus status, User user) {
+        this.id = id;
+        this.nom = nom;
+        this.description = description;
+        this.status = status;
+        this.user = user;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return Objects.equals(id, task.id) && Objects.equals(nom, task.nom) && Objects.equals(description, task.description) && status == task.status && Objects.equals(user, task.user);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nom, description, status, user);
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", nom='" + nom + '\'' +
+                ", description='" + description + '\'' +
+                ", status=" + status +
+                ", user=" + user +
+                '}';
+    }
+}
+
+
---
+++ src/main/java/com/todolist/portfolio/entity/TaskStatus.java
@@ -0,0 +1,9 @@
+package com.todolist.portfolio.entity;
+
+public enum TaskStatus {
+
+     TODO,
+     IN_PROGRESS,
+     DONE
+
+}
